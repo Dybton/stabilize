5d204c0c122da1072827503dd695f674118490dd
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-12-03 13:47:12 +0100

    installed nav

--- App.js
+++ App.js
@@ -2,10 +2,71 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {Testo} from './Testo';
 import { Test3 } from './Test3';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {GraphIcon} from './components/Icons/GraphIcon';
+import {AddIcon} from './components/Icons/AddIcon';
+import {BookIcon} from './components/Icons/BookIcon';
+
+
+const Stack  = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+  screenOptions={{
+    tabBarIcon: () => null, // This line removes the icon
+    headerShown: false,
+    tabBarStyle: {
+      height: 60, // Set your desired height here
+    },
+  }}
+>
+  <Tab.Screen 
+    name="Graph" 
+    component={Test3} 
+    options={{ 
+      tabBarIcon: ({ focused }) => (
+        <GraphIcon color={focused ? 'blue' : 'grey'} />
+      ),
+      tabBarShowLabel: false,
+    }}
+  />
+  <Tab.Screen 
+    name="Add" 
+    component={Testo} 
+    options={{ 
+      tabBarIcon: ({ focused }) => (
+        <View style={{marginBottom: 14}}>
+          <AddIcon color={focused ? 'blue' : 'grey'} />
+        </View>
+      ),
+      tabBarShowLabel: false,
+    }}
+  />
+  <Tab.Screen 
+    name="Notes" 
+    component={Testo} 
+    options={{ 
+      tabBarIcon: ({ focused }) => (
+        <BookIcon color={focused ? 'blue' : 'grey'} />
+      ),
+      tabBarShowLabel: false,
+    }}
+  />
+</Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
-      <Test3 />
+    <NavigationContainer>
+     <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/Icons/GraphIcon.js
@@ -0,0 +1,10 @@
+import { Svg, Rect, Path } from 'react-native-svg';
+
+export const GraphIcon = ({ color = 'black' }) => {
+  return (
+    <Svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 16 16" fill="none">
+      <Path d="M15 13V14H1.5L1 13.5V0H2V13H15Z" fill={color}/>
+      <Path d="M13 3.207L7.854 8.354H7.146L5.5 6.707L1.854 10.354L1.146 9.646L5.146 5.646H5.854L7.5 7.293L12.646 2.146H13.353L15.353 4.146L14.646 4.854L13 3.207Z" fill={color}/>
+    </Svg>
+  );
+}

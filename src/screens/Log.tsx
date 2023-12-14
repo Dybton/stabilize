daf0e2c77ef544d6f950da5248defed94aaf0490
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2023-12-14 21:05:42 +0100

    made progress on log

--- src/screens/Log.tsx
+++ src/screens/Log.tsx
@@ -28,20 +28,17 @@ const meals = [
   // Add more meals as needed
 ];
 
-// Main Log component
 const Log = () => {
   return (
     <ScrollView style={styles.container}>
-      <SleepSection sleepData={sleepData} />
+      <SleepComponent sleepData={sleepData} />
       <ActivitySection activities={activities} />
       <MealsSection meals={meals} />
-      {/* Add any additional sections or components here */}
     </ScrollView>
   );
 };
 
-// Subcomponent for displaying sleep information
-const SleepSection = ({ sleepData }) => (
+const SleepComponent = ({ sleepData }) => (
   <View style={styles.section}>
     <Text style={styles.title}>Sleep</Text>
     <View
@@ -53,43 +50,61 @@ const SleepSection = ({ sleepData }) => (
         alignItems: "center",
       }}
     >
-      <View>
+      <View style={{ flex: 1 }}>
         <BookIcon />
       </View>
 
-      <View>
+      <View style={{ flex: 3 }}>
         <Text>Total sleep </Text>
         <Text>{sleepData.total}</Text>
       </View>
 
-      <View>
+      <View style={{ flex: 2 }}>
         <Text>Quality </Text>
         <Text>{sleepData.quality}/10</Text>
       </View>
 
-      <View>
+      <View style={{ flex: 1.5 }}>
         <Text>Bed time</Text>
         <Text>{sleepData.bedTime}</Text>
-      </View>
-
-      <View>
-        <BookIcon />
       </View>
     </View>
   </View>
 );
 
-// Subcomponent for displaying activity information
 const ActivitySection = ({ activities }) => (
   <View style={styles.section}>
     <Text style={styles.title}>Activity</Text>
-    {activities.map((activity, index) => (
-      <View key={index} style={styles.banner}>
-        <Text>Training: {activity.name}</Text>
-        <Text>Duration: {activity.duration}</Text>
-        <Text>Time: {activity.time}</Text>
-      </View>
-    ))}
+    {activities.map(
+      (
+        activity: { name: string; duration: string; time: string },
+        index: number
+      ) => (
+        <View
+          key={index}
+          style={{
+            ...styles.banner,
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <View style={{ flex: 1 }}>
+            <BookIcon />
+          </View>
+
+          <View style={{ flex: 4 }}>
+            <Text>Activity:</Text>
+            <Text>{activity.name}</Text>
+          </View>
+
+          <View style={{ flex: 1.2, marginLeft: 5 }}>
+            <Text>Duration:</Text>
+            <Text>{activity.duration}</Text>
+          </View>
+        </View>
+      )
+    )}
   </View>
 );
 
@@ -97,10 +112,29 @@ const ActivitySection = ({ activities }) => (
 const MealsSection = ({ meals }) => (
   <View style={styles.section}>
     <Text style={styles.title}>Meals</Text>
-    {meals.map((meal, index) => (
-      <View key={index} style={styles.banner}>
-        <Text>Food: {meal.description}</Text>
-        <Text>Time: {meal.time}</Text>
+    {meals.map((meal: { description: string; time: string }, index: number) => (
+      <View
+        key={index}
+        style={{
+          ...styles.banner,
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <BookIcon />
+        </View>
+
+        <View style={{ flex: 4 }}>
+          <Text>Food:</Text>
+          <Text numberOfLines={2}>{meal.description}</Text>
+        </View>
+
+        <View style={{ flex: 1.2, marginLeft: 5 }}>
+          <Text>Time:</Text>
+          <Text>{meal.time}</Text>
+        </View>
       </View>
     ))}
   </View>
@@ -115,6 +149,8 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
     borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "red",
   },
   title: {
     fontSize: 20,
@@ -127,6 +163,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#e9e9e9",
     borderRadius: 5,
     height: 80,
+  },
+  box: {
+    borderWidth: 1,
+    borderColor: "red",
+    width: "50%",
   },
 });
 
